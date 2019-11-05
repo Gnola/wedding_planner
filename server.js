ff5a5a5df1c942d4c21548aaec88a88679d561e5
G <53386620+Gnola@users.noreply.github.com>
2019-11-05 09:01:37 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,47 @@
+// DEPENDENCIES //
+const express = require('express');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const app = express()
+const db = mongoose.connection
+require('dotenv').config()
+
+// PORT //
+const PORT = process.env.PORT // Allow use of Heroku's port or your own local port, depending on the environment
+  // console.log(PORT);
+
+
+// DATABASE //
+const MONGODB_URI = process.env.MONGODB_URI
+  // console.log(MONGODB_URI);
+
+// CONNECT TO MONGO //
+mongoose.connect(
+  MONGODB_URI ,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  });
+
+// SUCCESS / ERROR //
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+
+// MIDDLEWARE //
+app.use(express.static('public')) // use publuc folder for static assets
+app.use(express.urlencoded({extended:false})); // pupulate req.body with parsed info from forms
+app.use(methodOverride('_method')); // be able to use DELETE and PUT routes
+
+
+// ROUTES //
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+// LISTENER //
+app.listen(PORT, () => {
+  console.log('listening...');
+})
