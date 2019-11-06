8eb7539e9846f0e1f1d3647bbab54294dc3ed013
G <53386620+Gnola@users.noreply.github.com>
2019-11-06 11:27:26 -0800

    added rsvp functionality

--- server.js
+++ server.js
@@ -1,33 +1,26 @@
 // DEPENDENCIES //
-const express = require('express');
-const app = express()
-const methodOverride = require('method-override');
-const mongoose = require('mongoose');
-const db = mongoose.connection
-require('dotenv').config()
-const session = require('express-session');
+const express = require('express'); // use express
+const app = express() // use express
+const methodOverride = require('method-override'); // use methodOverride
+const mongoose = require('mongoose'); // use mongoose
+const session = require('express-session'); // use sessions / cookies
+require('dotenv').config() // use dotenv
 
-// PORT //
-const PORT = process.env.PORT // Allow use of Heroku's port or your own local port, depending on the environment
+const PORT = process.env.PORT // PORT - Allow use of Heroku's port or your own local port, depending on the environment
+const MONGODB_URI = process.env.MONGODB_URI // DATABASE
 
-// DATABASE //
-const MONGODB_URI = process.env.MONGODB_URI
-
-// SUCCESS / ERROR //
-db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
-db.on('disconnected', () => console.log('mongo disconnected'));
+// // SUCCESS / ERROR //
+// const db = mongoose.connection
+// db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+// db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+// db.on('disconnected', () => console.log('mongo disconnected'));
 
 
 // MIDDLEWARE //
-app.use(express.static('public')) // use publuc folder for static assets
-app.use(express.urlencoded({extended:false})); // pupulate req.body with parsed info from forms
+app.use(express.static('public')) // use public folder for static assets == CSS
+app.use(express.urlencoded({extended:false})); // populate req.body with parsed info from forms
 app.use(methodOverride('_method')); // be able to use DELETE and PUT routes
-app.use(session({ // set up session cookies
-  secret: 'randomstring',
-  resave: false,
-  saveUninitialized:false
-}))
+app.use(session({ secret: 'randomstring',resave: false, saveUninitialized:false})) // set up session cookies
 
 
 // CONTROLLER CONNECTIONS //
@@ -41,28 +34,30 @@ const sessionController = require('./controllers/session.js') // USERS
 app.use('/sessions', sessionController)
 
 
+
 // ROUTES //
 app.get('/', (req, res) => { // WELCOME.EJS
   res.render('welcome.ejs') // Sign up or Log in
 })
 
-app.get('/set', (req, res) => {
+app.get('/set', (req, res) => { // SET UP COOKIE
   req.session.username = "cookie";
   res.send('I set a cookie')
 })
 
-app.get('/get', (req, res) => {
+app.get('/get', (req, res) => { // SEE COOKIE
   res.send(req.session.username)
 })
 
-app.get('/logout', (req, res)=>{
-	req.session.destroy((err)=>{
-		if(err){
-			res.redirect('/guests')
-		} else {
-      res.redirect('/')
-		}
-	});
+app.get('/logout', (req, res)=>{ // LOG OUT
+	req.session.destroy(
+    (err)=>{
+	    if (err) {
+		    res.redirect('/guests')
+	    } else {
+        res.redirect('/')
+      }
+	  });
 });
 
 
@@ -75,7 +70,11 @@ mongoose.connect(
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
-  });
+  },
+  () => {
+    console.log('connected to mongoose');
+  }
+);
 
 
 // LISTENER //
