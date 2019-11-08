cc5951c39758e59314c70e6607803ebd36c45d0e
G <53386620+Gnola@users.noreply.github.com>
2019-11-08 13:38:48 -0800

    added more info to guests

--- models/guests.js
+++ models/guests.js
@@ -6,7 +6,14 @@ const guestSchema = new mongoose.Schema ({
   name: {type:String, required: true},
   plusOne: {type:String, default: 'None'},
   rsvp: {type:String, default: 'Not sure yet'},
-  category: {type:String, default: 'None'}
+  category: {type:String, default: 'None'},
+  email: String,
+  phone: Number,
+  street: String,
+  city: String,
+  state: String,
+  zip: Number,
+  // invited: Boolean,
 })
 
 const Guest = mongoose.model('Guest', guestSchema);
