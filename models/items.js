04cdce3fcde3ed4639edb98ccb0bf97e6995102a
G <53386620+Gnola@users.noreply.github.com>
2019-11-09 14:17:13 -0800

    added styling to budget pages

--- models/items.js
+++ models/items.js
@@ -2,10 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const itemSchema = new mongoose.Schema ({
-  service: String,
-  price: Number,
-  paid: String,
-  notes: String
+  service: {type:String, default:''},
+  price: {type:Number, default: 0, min: 0},
+  paid: {type:String, default: "No"},
+  notes: {type:String, default: ''}
 })
 
 const Item = mongoose.model('Item', itemSchema);
